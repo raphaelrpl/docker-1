4c6dfa30f1a370cb0c4082039fc8c6e2add2daa3
raphaelrpl <raphael.wcosta@gmail.com>
2017-12-12 16:35:55 -0200

    Update geoserver

--- geoserver/docker-entrypoint.sh
+++ geoserver/docker-entrypoint.sh
@@ -5,29 +5,22 @@ if [[ $GEOSERVER_URL != /* ]]; then
   exit 1
 fi
 
-URL_NAME=$(echo $GEOSERVER_URL | cut -d "/" -f 2)
-XML_FILE=/var/lib/tomcat8/conf/Catalina/localhost/$URL_NAME.xml
+sed "143c<Context path=\"$GEOSERVER_URL\" docBase=\"${APP_DATA_DIR}/geoserver.war\"/>" /var/lib/tomcat8/conf/server.xml > /tmp/server.xml && \
+mv /tmp/server.xml /var/lib/tomcat8/conf/server.xml
 
-if [ ! -e $XML_FILE ]; then
-  echo "Creating configuration file $XML_FILE ... "
-  # Updating geoserver path
-  echo "<?xml version='1.0' encoding='UTF-8'?>
-  <Context path=\"$GEOSERVER_URL\" docBase=\"/opt/app/geoserver.war\"/>" >> /var/lib/tomcat8/conf/Catalina/localhost/$URL_NAME.xml
-fi
+# if [ -z "$TOMCAT_CORS" ]; then
+#   echo "Enabling cors : $TOMCAT_CORS"
 
-if [ -z "$TOMCAT_CORS" ]; then
-  echo "Enabling cors : $TOMCAT_CORS"
+#   sed -i "478i/<filter>\n
+#                  <filter-name>CorsFilter</filter-name>\n\
+#                  <filter-class>org.apache.catalina.filters.CorsFilter</filter-class>\n\
+#                </filter>\n\
+#                <filter-mapping>\n\
+#                  <filter-name>CorsFilter</filter-name>\n\
+#                  <url-pattern>/*</url-pattern>\n\
+#                </filter-mapping>" /usr/local/tomcat/conf/web.xml
 
-  sed -i "478i/<filter>\n
-                 <filter-name>CorsFilter</filter-name>\n\
-                 <filter-class>org.apache.catalina.filters.CorsFilter</filter-class>\n\
-               </filter>\n\
-               <filter-mapping>\n\
-                 <filter-name>CorsFilter</filter-name>\n\
-                 <url-pattern>/*</url-pattern>\n\
-               </filter-mapping>" /usr/local/tomcat/conf/web.xml
-
-fi
+# fi
 
 service tomcat8 start
 
