29e6cdbcc143d9b9faa9fa2192258b4bc30c945d
raphaelrpl <raphael.wcosta@gmail.com>
2017-12-12 13:28:20 -0200

    Update GeoServer to ubuntu 16.04 in order to solve enconding

--- geoserver/docker-entrypoint.sh
+++ geoserver/docker-entrypoint.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-# Updating geoserver path
-sed "125c<Context path=\"$GEOSERVER_URL\" docBase=\"/root/geoserver.war\"/>" /usr/local/tomcat/conf/server.xml > /root/server.xml
-mv /root/server.xml /usr/local/tomcat/conf/server.xml
+if [[ $GEOSERVER_URL != /* ]]; then
+  echo "A GEOSERVER_URL must starts with slash."
+  exit 1
+fi
 
-catalina.sh run
+URL_NAME=$(echo $GEOSERVER_URL | cut -d "/" -f 2)
+XML_FILE=/var/lib/tomcat8/conf/Catalina/localhost/$URL_NAME.xml
+
+if [ ! -e $XML_FILE ]; then
+  echo "Creating configuration file $XML_FILE ... "
+  # Updating geoserver path
+  echo "<?xml version='1.0' encoding='UTF-8'?>
+  <Context path=\"$GEOSERVER_URL\" docBase=\"/opt/app/geoserver.war\"/>" >> /var/lib/tomcat8/conf/Catalina/localhost/$URL_NAME.xml
+fi
+
+if [ -z "$TOMCAT_CORS" ]; then
+  echo "Enabling cors : $TOMCAT_CORS"
+
+  sed -i "478i/<filter>\n
+                 <filter-name>CorsFilter</filter-name>\n\
+                 <filter-class>org.apache.catalina.filters.CorsFilter</filter-class>\n\
+               </filter>\n\
+               <filter-mapping>\n\
+                 <filter-name>CorsFilter</filter-name>\n\
+                 <url-pattern>/*</url-pattern>\n\
+               </filter-mapping>" /usr/local/tomcat/conf/web.xml
+
+fi
+
+service tomcat8 start
+
+tail -f /var/log/tomcat8/catalina.out
